f758481d5b3813314436b5a2a54485bb31557e7d
RomanK <roman.kvasnikov@gmail.com>
2026-03-08 19:46:09 +0300

    2026-03-08 19:46:09

--- system/modules/networking.nix
+++ system/modules/networking.nix
@@ -2,6 +2,9 @@
   networking = {
     hostName = hostname;
 
+    proxy.default = "socks://127.0.0.1:10808";
+    proxy.noProxy = "localhost,127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12";
+
     networkmanager = {
       enable = true;
 
